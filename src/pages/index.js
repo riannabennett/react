21cedae67b600f3284d75090a90d0b4298fb7b09
riannabennett <riannakbennett@gmail.com>
2018-08-31 11:30:04 -0400

    intial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hello. <br />I'm Rye, an architect turned product designer living in Brooklyn.</h1>
-    <p>I'm currently building products at Meetup. I have experience with startups, UI/UX, design strategy, product management, running my own freelance design shop, and all things food.</p>
+    <p>I'm currently building products at Meetup. I have experience with startups, UI/UX, design strategy, product management, running my own freelance design shop, and all things food. Test</p>
     <Link to="/page-2/">Learn more.</Link>
   </div>
 )
