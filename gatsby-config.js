274ab3f69b061be22f507f3cbec864fb82354828
rbennett <rbennett@rbennett-mbp.lan>
2018-08-31 11:09:08 -0400

    intial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Rye Bennett',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello. <br />I'm Rye, an architect turned product designer living in Brooklyn.</h1>
+    <p>I'm currently building products at Meetup. I have experience with startups, UI/UX, design strategy, product management, running my own freelance design shop, and all things food.</p>
+    <Link to="/page-2/">Learn more.</Link>
+  </div>
+)
+
+export default IndexPage
